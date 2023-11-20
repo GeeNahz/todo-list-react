6c06fa140949dfde5bf78032625727ee74ef9316
Godswill Nasara <godswillnlm@gmail.com>
2023-11-20 22:57:40 +0100

    Feature: create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "../styles/header.css";
+
+const Header = () => {
+    return (
+        <nav className="navbar">
+            <div className="logo">Logo</div>
+            <div className="links">links</div>
+        </nav>
+    )
+}
+
+export default Header;
